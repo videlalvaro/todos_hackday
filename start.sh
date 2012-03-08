400755f655f8bc0896d4247becae5d2ebd70d1c7
Alvaro Videla <videlalvaro@gmail.com>
2012-03-08 01:02:39 +0100

    updates app startup

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s hackday
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s hackday -config ./priv/hackday
+
